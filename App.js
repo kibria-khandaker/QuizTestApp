80be57a9c7fc1578b23ef5cca84ed864119619e0
kibria-khandaker <kibriakhandaker@gmail.com>
2025-12-20 21:14:46 +0300

    QuizTest project Done 1st Step.

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import QuizScreen from './src/screens/QuizScreen';
 import ResultScreen from './src/screens/ResultScreen';
 
@@ -6,20 +8,22 @@ export default function App() {
   const [score, setScore] = useState(0);
   const [quizFinished, setQuizFinished] = useState(false);
 
+  const finishQuiz = () => setQuizFinished(true);
   const restartQuiz = () => {
     setScore(0);
     setQuizFinished(false);
   };
 
-  return quizFinished ? (
-    <ResultScreen
-      score={score}
-      restartQuiz={restartQuiz}
-    />
-  ) : (
-    <QuizScreen
-      setScore={setScore}
-      finishQuiz={setQuizFinished}
-    />
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        {!quizFinished ? (
+          <QuizScreen setScore={setScore} finishQuiz={finishQuiz} />
+        ) : (
+          <ResultScreen score={score} restartQuiz={restartQuiz} />
+        )}
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
